6b9597ea3206991a655c9b19d7e4d74b328cdfe5
Carlo Jimenez <jimenez.carlo.llabor@gmail.com>
2022-10-14 18:31:28 +0800

    update db

--- database/gym_db.sql
+++ database/gym_db.sql
@@ -1,34 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 11, 2021 at 07:32 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: gym_db
+-- ------------------------------------------------------
+-- Server version	10.4.22-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `gym_db`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `members`
 --
 
+DROP TABLE IF EXISTS `members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `members` (
-  `id` int(30) NOT NULL,
-  `member_id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
+  `member_id` int(30) unsigned NOT NULL,
   `firstname` varchar(100) NOT NULL,
   `middlename` varchar(100) NOT NULL,
   `lastname` varchar(100) NOT NULL,
@@ -36,266 +32,221 @@ CREATE TABLE `members` (
   `contact` varchar(100) NOT NULL,
   `address` text NOT NULL,
   `email` varchar(100) NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `members`
 --
 
-INSERT INTO `members` (`id`, `member_id`, `firstname`, `middlename`, `lastname`, `gender`, `contact`, `address`, `email`, `date_created`) VALUES
-(7, 103197, 'Angel Jude', 'R', 'Suarez', 'Male', '09569666494', 'Bacolod', 'suarez@yahoo.com', '2021-03-10 11:41:06'),
-(10, 199087, 'Nikko', 'A', 'Curaza', 'Male', '09051223198', 'Secret', 'nikko@yahoo.com', '2021-03-10 11:41:06'),
-(11, 199233, 'Arnulfo', 'A', 'Rebollosa', 'Male', '09058728883', 'Secret', 'reb@yahoo.com', '2021-03-10 11:41:06');
-
--- --------------------------------------------------------
+LOCK TABLES `members` WRITE;
+/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+INSERT INTO `members` VALUES (7,103197,'Angel Jude','R','Suarez','Male','09569666494','Bacolod','suarez@yahoo.com','2021-03-10 11:41:06'),(11,199233,'Arnulfo','A','Rebollosa','Male','09058728883','Secret','reb@yahoo.com','2021-03-10 11:41:06'),(18,103197,'member','member','member','Male','09217635295','member','member@gmail.com','2022-10-14 15:11:45'),(19,103197,'member','member','member','Male','09000000000','member','member@gmail.com','2022-10-14 18:14:36');
+/*!40000 ALTER TABLE `members` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `packages`
 --
 
+DROP TABLE IF EXISTS `packages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `packages` (
-  `id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `package` varchar(200) NOT NULL,
   `description` text NOT NULL,
-  `amount` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `amount` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `packages`
 --
 
-INSERT INTO `packages` (`id`, `package`, `description`, `amount`) VALUES
-(4, 'Premium Package', 'Sample Package', 1000);
-
--- --------------------------------------------------------
+LOCK TABLES `packages` WRITE;
+/*!40000 ALTER TABLE `packages` DISABLE KEYS */;
+INSERT INTO `packages` VALUES (4,'Premium Package','Sample Package',1000),(5,'Starter Package','Sample Package\r\n\r\n',650);
+/*!40000 ALTER TABLE `packages` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `payments`
 --
 
+DROP TABLE IF EXISTS `payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `payments` (
-  `id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `registration_id` int(30) NOT NULL,
   `amount` int(30) NOT NULL,
   `remarks` text NOT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT '2' COMMENT '1=registration, 2= monthly payment',
-  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=registration, 2= monthly payment',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `payments`
 --
 
-INSERT INTO `payments` (`id`, `registration_id`, `amount`, `remarks`, `type`, `date_created`) VALUES
-(1, 2, 4500, 'First payment', 2, '2020-10-21 14:39:26'),
-(2, 2, 3500, 'payment for november', 2, '2020-10-21 14:39:52'),
-(3, 6, 5000, 'good', 2, '2021-03-10 12:28:46');
-
--- --------------------------------------------------------
+LOCK TABLES `payments` WRITE;
+/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
+INSERT INTO `payments` VALUES (1,2,4500,'First payment',2,'2020-10-21 14:39:26'),(2,2,3500,'payment for november',2,'2020-10-21 14:39:52'),(3,6,5000,'good',2,'2021-03-10 12:28:46'),(4,9,100,'test',2,'2022-10-14 15:14:34');
+/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `plans`
 --
 
+DROP TABLE IF EXISTS `plans`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `plans` (
-  `id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `plan` int(30) NOT NULL,
-  `amount` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `amount` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `plans`
 --
 
-INSERT INTO `plans` (`id`, `plan`, `amount`) VALUES
-(1, 12, 1000);
-
--- --------------------------------------------------------
+LOCK TABLES `plans` WRITE;
+/*!40000 ALTER TABLE `plans` DISABLE KEYS */;
+INSERT INTO `plans` VALUES (1,12,1000),(2,6,600);
+/*!40000 ALTER TABLE `plans` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `registration_info`
 --
 
+DROP TABLE IF EXISTS `registration_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `registration_info` (
-  `id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `member_id` int(30) NOT NULL,
   `plan_id` int(30) NOT NULL,
   `package_id` int(30) NOT NULL,
   `start_date` date NOT NULL,
   `end_date` date NOT NULL,
   `trainer_id` tinyint(30) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0=Inactive, 1= Active',
-  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0=Inactive, 1= Active',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `registration_info`
 --
 
-INSERT INTO `registration_info` (`id`, `member_id`, `plan_id`, `package_id`, `start_date`, `end_date`, `trainer_id`, `status`, `date_created`) VALUES
-(2, 5, 1, 2, '2020-10-21', '2021-10-21', 0, 0, '2020-10-21 00:00:00'),
-(3, 5, 1, 2, '2020-10-21', '2021-10-21', 0, 0, '2020-10-21 00:00:00'),
-(4, 6, 1, 2, '2019-10-19', '2020-10-19', 0, 0, '2020-10-21 00:00:00'),
-(5, 6, 1, 2, '2020-10-21', '2021-10-21', 0, 1, '2020-10-21 00:00:00'),
-(6, 7, 1, 2, '2021-03-10', '2022-03-10', 2, 0, '2021-03-10 12:27:18'),
-(7, 5, 1, 2, '2021-03-10', '2022-03-10', 2, 0, '2021-03-10 12:27:49'),
-(8, 11, 1, 4, '2021-03-10', '2022-03-10', 2, 0, '2021-03-10 14:13:29');
-
--- --------------------------------------------------------
+LOCK TABLES `registration_info` WRITE;
+/*!40000 ALTER TABLE `registration_info` DISABLE KEYS */;
+INSERT INTO `registration_info` VALUES (2,5,1,2,'2020-10-21','2021-10-21',0,0,'2020-10-21 00:00:00'),(3,5,1,2,'2020-10-21','2021-10-21',0,0,'2020-10-21 00:00:00'),(4,6,1,2,'2019-10-19','2020-10-19',0,0,'2020-10-21 00:00:00'),(5,6,1,2,'2020-10-21','2021-10-21',0,1,'2020-10-21 00:00:00'),(6,7,1,2,'2021-03-10','2022-03-10',2,0,'2021-03-10 12:27:18'),(7,5,1,2,'2021-03-10','2022-03-10',2,0,'2021-03-10 12:27:49'),(8,11,1,4,'2021-03-10','2022-03-10',2,0,'2021-03-10 14:13:29'),(9,18,1,4,'2022-10-14','2023-10-14',0,0,'2022-10-14 15:13:41'),(10,18,1,4,'2022-10-14','2023-10-14',2,1,'2022-10-14 15:14:46');
+/*!40000 ALTER TABLE `registration_info` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `schedules`
 --
 
+DROP TABLE IF EXISTS `schedules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `schedules` (
-  `id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `member_id` int(30) NOT NULL,
   `dow` text NOT NULL,
   `date_from` date NOT NULL,
   `date_to` date NOT NULL,
   `time_from` time NOT NULL,
-  `time_to` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `time_to` time NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `schedules`
+--
+
+LOCK TABLES `schedules` WRITE;
+/*!40000 ALTER TABLE `schedules` DISABLE KEYS */;
+INSERT INTO `schedules` VALUES (2,18,'0','2022-10-01','2022-10-31','15:15:00','03:15:00');
+/*!40000 ALTER TABLE `schedules` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `trainers`
 --
 
+DROP TABLE IF EXISTS `trainers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `trainers` (
-  `id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `contact` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `rate` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `rate` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `trainers`
 --
 
-INSERT INTO `trainers` (`id`, `name`, `contact`, `email`, `rate`) VALUES
-(2, 'Adones Evangelista', '09098723987', 'adones@gmail.com', 500);
-
--- --------------------------------------------------------
+LOCK TABLES `trainers` WRITE;
+/*!40000 ALTER TABLE `trainers` DISABLE KEYS */;
+INSERT INTO `trainers` VALUES (2,'Adones Evangelista','09098723987','adones@gmail.com',500),(3,'Trainer','09217635295','trainer@gmail.com',300);
+/*!40000 ALTER TABLE `trainers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `username` varchar(200) NOT NULL,
   `password` text NOT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT '3' COMMENT '1=Admin,2=Staff, 3= subscriber'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `type` tinyint(1) NOT NULL DEFAULT 3 COMMENT '1=Admin,2=Staff, 3= subscriber',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
-(2, 'Angel Jude Suarez', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'Admin1231','admin','202cb962ac59075b964b07152d234b70',1),(3,'test','test','202cb962ac59075b964b07152d234b70',1),(6,'test test test','test','202cb962ac59075b964b07152d234b70',1),(7,'a','a','0cc175b9c0f1b6a831c399e269772661',1),(8,'b','b','92eb5ffee6ae2fec3ad71c777531578f',1),(9,'c','c','202cb962ac59075b964b07152d234b70',2);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `members`
---
-ALTER TABLE `members`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `packages`
---
-ALTER TABLE `packages`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `payments`
---
-ALTER TABLE `payments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `plans`
---
-ALTER TABLE `plans`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `registration_info`
---
-ALTER TABLE `registration_info`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `schedules`
---
-ALTER TABLE `schedules`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `trainers`
---
-ALTER TABLE `trainers`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `members`
---
-ALTER TABLE `members`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
--- AUTO_INCREMENT for table `packages`
---
-ALTER TABLE `packages`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `payments`
---
-ALTER TABLE `payments`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `plans`
---
-ALTER TABLE `plans`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `registration_info`
---
-ALTER TABLE `registration_info`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `schedules`
---
-ALTER TABLE `schedules`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `trainers`
---
-ALTER TABLE `trainers`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-14 18:30:32
