324e8391e9e73abbbd30ef2df24e738b70cca757
Carlo Jimenez <jimenez.carlo.llabor@gmail.com>
2022-11-04 19:40:09 +0800

    fixed modules

--- js/user-page.js
+++ js/user-page.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
       $('a.sidebar-btn').removeClass('active');
         $(this).addClass('active');
         $(".result").html('');
-        $("#content").load(base_url + 'module/page.php?page=' + page);
+        $("#content").load(base_url + 'page.php?page=' + page);
         localStorage.setItem('page', page);
       });
   
@@ -30,7 +30,7 @@ $(document).ready(function() {
       page += (path.has('id')) ? "&id="+path.get('id') : "";
       
       $(".result").html('');
-      $("#content").load(base_url + 'module/page.php?page=' + page);
+      $("#content").load(base_url + 'page.php?page=' + page);
     }
   }
 });
@@ -48,7 +48,7 @@ $(document).on("click", '.btn-edit, .btn-view', function () {
   var id = $(this).attr('value');
   
   $(".result").html('');
-  $("#content").load(base_url + 'module/page.php?page=' + page + '&id=' + id);
+  $("#content").load(base_url + 'page.php?page=' + page + '&id=' + id);
   
 });
 
@@ -82,62 +82,62 @@ $(document).on("submit", 'form', function (e) {
       }
       if (result.status == true) {
        if (form_name == 'update_user' && type_value == 'delete_user') {
-         $( "#content" ).load( base_url+'module/page.php?page=users' );
+         $( "#content" ).load( base_url+'page.php?page=users' );
        }
        if (form_name == 'update_user' && type_value == 'update' && formdata.get('access') == null) {
-         $( "#content" ).load( base_url+'module/page.php?page=customer_edit&id=' +formdata.get('user_id'));
+         $( "#content" ).load( base_url+'page.php?page=customer_edit&id=' +formdata.get('user_id'));
        }
        if (form_name == 'update_user' && type_value == 'update' && formdata.get('access')) {
-         $( "#content" ).load( base_url+'module/page.php?page=user_edit&id=' +formdata.get('user_id'));
+         $( "#content" ).load( base_url+'page.php?page=user_edit&id=' +formdata.get('user_id'));
        }
        if (form_name == 'update_user' && type_value == 'delete_customer') {
-         $( "#content" ).load( base_url+'module/page.php?page=customers' );
+         $( "#content" ).load( base_url+'page.php?page=customers' );
        }
        if (form_name == 'remove_from_cart' || form_name == 'update_cart' || form_name == 'checkout_cart') {
-         $( "#content" ).load( base_url+'module/page.php?page=cart' );
+         $( "#content" ).load( base_url+'page.php?page=cart' );
         }
         
        if (form_name == 'update_transaction' && type_value == '5') {
-         $( "#content" ).load( base_url+'module/page.php?page=customer_orders' );
+         $( "#content" ).load( base_url+'page.php?page=customer_orders' );
         }
        if (form_name == 'update_transaction' && (type_value == '3' || type_value == '6')) {
-         $( "#content" ).load( base_url+'module/page.php?page=transactions' );
+         $( "#content" ).load( base_url+'page.php?page=transactions' );
         }
        if (form_name == 'update_transaction_view' && (type_value == '3' || type_value == '6')) {
-         $( "#content" ).load( base_url+'module/page.php?page=transaction_view&id=' +formdata.get('id'));
+         $( "#content" ).load( base_url+'page.php?page=transaction_view&id=' +formdata.get('id'));
         }
         
         if (form_name == 'update_order' && (type_value == '3' || type_value == '6')) {
-         $( "#content" ).load( base_url+'module/page.php?page=orders' );
+         $( "#content" ).load( base_url+'page.php?page=orders' );
         }
        if (form_name == 'update_orders_view' && (type_value == '3' || type_value == '6')) {
-         $( "#content" ).load( base_url+'module/page.php?page=orders_view&id=' +formdata.get('id'));
+         $( "#content" ).load( base_url+'page.php?page=orders_view&id=' +formdata.get('id'));
         }
        if (form_name == 'update_order_transaction' && (type_value == '3' || type_value == '6')) {
-         $( "#content" ).load( base_url+'module/page.php?page=orders_view&id=' +formdata.get('invoice_id'));
+         $( "#content" ).load( base_url+'page.php?page=orders_view&id=' +formdata.get('invoice_id'));
         }
         
        if (form_name == 'update_category' && type_value == 'delete') {
-         $( "#content" ).load( base_url+'module/page.php?page=categories' );
+         $( "#content" ).load( base_url+'page.php?page=categories' );
         }
         if (form_name == 'update_category' && type_value == 'update') {
-         $( "#content" ).load( base_url+'module/page.php?page=category_edit&id='+formdata.get('category_id') );
+         $( "#content" ).load( base_url+'page.php?page=category_edit&id='+formdata.get('category_id') );
         }
         
        if (form_name == 'add_product') {
          $('#preview').attr('src','images/products/default.png');
        }
        if (form_name == 'update_product' && type_value == 'delete') {
-         $( "#content" ).load( base_url+'module/page.php?page=products' );
+         $( "#content" ).load( base_url+'page.php?page=products' );
        }
        if (form_name == 'update_product' && type_value == 're_stock_list') {
-         $( "#content" ).load( base_url+'module/page.php?page=inventory' );
+         $( "#content" ).load( base_url+'page.php?page=inventory' );
        }
        if (form_name == 'update_product' && type_value == 're_stock') {
-         $( "#content" ).load( base_url+'module/page.php?page=inventory_edit&id='+formdata.get('product_id') );
+         $( "#content" ).load( base_url+'page.php?page=inventory_edit&id='+formdata.get('product_id') );
         }
        if (form_name == 'update_product' && type_value == 'update') {
-         $( "#content" ).load( base_url+'module/page.php?page=product_edit&id='+formdata.get('product_id') );
+         $( "#content" ).load( base_url+'page.php?page=product_edit&id='+formdata.get('product_id') );
         }
       }
       if (result.items != '') {
